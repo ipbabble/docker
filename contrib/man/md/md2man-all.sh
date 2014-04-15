8aa26d8f89b109c00fcdc6d286208620c57e1c53
Sven Dowideit <SvenDowideit@home.org.au>
2014-04-15 11:43:27 +0000

    add a Dockerfile to do the conversion

--- contrib/man/md/md2man-all.sh
+++ contrib/man/md/md2man-all.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 mkdir -p ../man1
-for FILE in docker*.md; do pandoc -s -t man $FILE -o ../man1/"${FILE%.*}".1; done
+for FILE in docker*.md; do pandoc -s -t man $FILE -o ../man1/"${FILE%.*}".1; echo "converted ../man1/${FILE%.*}.1"; done
